ef7a965ff88d459aa88a7391cd122fa0a7af7635
pooja dallakoti <pujadallakoti82@gmail.com>
2025-12-05 22:21:19 +0545

    update page

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -19,6 +19,13 @@
                         echo $_SESSION['delete'];
                         unset ($_SESSION['delete']);
                     }
+
+                     if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset ($_SESSION['update']);
+                    }
+
                 ?>
 
                 <br><br><br>
@@ -73,7 +80,7 @@
                         <td> <?php echo $full_name; ?> </td>
                         <td> <?php echo $username; ?> </td>
                         <td>
-                           <a href="#" class="btn-secondary"> Update Admin</a>
+                           <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary"> Update Admin</a>
                             <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
                         </td>
                     </tr>
